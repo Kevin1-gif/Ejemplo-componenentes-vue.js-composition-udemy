6d867970ef40f04f6a72f52c2a66edb22b87d798
kevin muñoz <kevin@touchesbegan.com>
2024-12-13 11:45:42 -0600

    Post favoritos

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ejemplo curso udemy Vue 3
+
+- [Preview page](https://stirring-chimera-7b5cde.netlify.app/)
+
+Este es un ejemplo del curso de udemy de vue.js 3, trabajando con componentes y el composition api.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(["inicio", "find", "maxLength"])
+const emit = defineEmits(['next', 'prev'])
+</script>
+
+<template>
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button @click="$emit('prev')" type="button" class="btn btn-outline-primary" :disabled="inicio <=0">Previus {{ inicio }}</button>
+  <button @click="$emit('next')"  type="button" class="btn btn-outline-primary" :disabled="find >= maxLength">Next {{ find }}</button>
+</div>
+</template>
